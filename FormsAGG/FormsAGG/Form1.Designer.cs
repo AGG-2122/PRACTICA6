5ce976c0190da5bf381b1f09bf9cc0c953d4d3e6
AGG-2122 <104921444+AGG-2122@users.noreply.github.com>
2022-05-07 21:21:01 +0200

    Primera_Version_AGG

--- FormsAGG/FormsAGG/Form1.Designer.cs
+++ FormsAGG/FormsAGG/Form1.Designer.cs
@@ -72,9 +72,9 @@ namespace FormsAGG
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(456, 32);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.Size = new System.Drawing.Size(32, 13);
             this.label3.TabIndex = 3;
-            this.label3.Text = "Telefono";
+            this.label3.Text = "Móvil";
             // 
             // label4
             // 
